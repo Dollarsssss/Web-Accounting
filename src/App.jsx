c38dd371d3df72970291b3068bbccf0f001c0db2
Aomsup Pongpan <dollars2869@gmail.com>
2024-02-03 16:16:57 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,31 @@ import { useState } from 'react'
 import './App.css'
 import Transection from './components/Transection';
 import FormComponents from './components/formComponents';
-import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const design = {color:'red' , textAlign:'center'}
-
-  const data = [
-    {id:1,title :"ค่ารักษา",amout:10000 ,isLogic:false},
-    {id:2,title :"ค่าของใช้",amout:500 ,isLogic:true},
-    {id:3,title :"ค่าเกม",amout:8000,isLogic:false}
+  
+  const initData = [
+    {id:1,title :"ค่ารักษา",amout:10000},
+    {id:2,title :"ค่าของใช้",amout:500},
+    {id:3,title :"ค่าเกม",amout:8000}
   ]
+
+  const [items, setItems] = useState([])
+
+  function onAddNewItem(newItem){
+    setItems((prevItem)=>{
+      return [newItem,...prevItem]
+    });
+    console.log('Info form Components ', newItem);
+  }
+
 
   return (
     <div className="container">
-      <h1 style={design}>แอพรายรับ - รายจ่าย</h1>
-      <FormComponents/>
-      <Transection/>
+      <h1 style={{color:'red' , textAlign:'center'}}>แอพรายรับ - รายจ่าย</h1>
+      <FormComponents onAddItem ={onAddNewItem}/>
+      <Transection items ={items}/>
 
     </div>
 
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-const Item = ({title ,isLogic , amout}) => {
+const Item = ({title , amout}) => {
 
     return (
-        <li style={{color: isLogic ? "magenta" : "darkgreen"}}>{title}<span>{amout}</span></li>
+        <li>{title}<span>{amout}</span></li>
     )
 }
 
 
 Item.propTypes = {
     title : PropTypes.string.isRequired,
-    isLogic : PropTypes.bool.isRequired,
     amout : PropTypes.number.isRequired
 }
 export default Item;
