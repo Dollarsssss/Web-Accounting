cb27b7704d1934754f1df53255546701a7361229
Aomsup Pongpan <dollars2869@gmail.com>
2024-02-03 15:30:45 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import { useState } from 'react'
 import './App.css'
 import Transection from './components/Transection';
 import FormComponents from './components/formComponents';
-
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [count, setCount] = useState(0)
   const design = {color:'red' , textAlign:'center'}
 
+  const data = [
+    {id:1,title :"ค่ารักษา",amout:10000 ,isLogic:false},
+    {id:2,title :"ค่าของใช้",amout:500 ,isLogic:true},
+    {id:3,title :"ค่าเกม",amout:8000,isLogic:false}
+  ]
+
   return (
     <div className="container">
       <h1 style={design}>แอพรายรับ - รายจ่าย</h1>
       <FormComponents/>
-      <Transection />
+      <Transection/>
 
     </div>
 
