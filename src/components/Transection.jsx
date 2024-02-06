4c30f14fe1a3aeb1d9f749498f7e3b64fa56354d
Aomsup Pongpan <dollars2869@gmail.com>
2024-02-06 17:23:53 +0700

    UseContext

--- src/components/Transection.jsx
+++ src/components/Transection.jsx
@@ -1,17 +1,18 @@
 import Item from './Item'
 import React from "react";
-
 import './Transection.css'
 
 const Transection = ({ items }) => {
 
 
     return (
+      <>
       <ul className="text-item">
         {items.map(items =>{
           return <Item key={items.id}{...items}/>
         })}
       </ul>
+      </>
     );
   }
 
