3a2ef91d9ccfc4f073eb4804fb7b39ec5580efaa
Dollars <aomsup5185@gmail.com>
2024-02-08 15:46:16 +0700

    Update Report Style

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,15 +1,25 @@
 import { React , useContext }from "react";
 import DataContext from "../data/DataContext";
+import './Report.css'
 
 function Report() {
 
      const {income,expense} = useContext(DataContext)
 
   return (
-    <div>
-        <p>รายรับ :{income}</p>
-        <p>รายจ่าย :{expense}</p>
-    </div>
+    <>
+       <h4>ยอดคงเหลือ : {income-expense}</h4>
+       <div className="report-container">
+          <div>
+            <h4>รายได้ทั้งหมด</h4>
+            <p className="report-income">{income}</p>
+          </div>
+          <div>
+          <h4>รายจ่ายทั้งหมด</h4>
+            <p className="report-expense">{expense}</p>
+          </div>
+       </div>
+    </>
   )
 }
 
